604f6f604b4b4e69362a4ed2fb08c755cd1792d7
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-09-08 19:15:48 +0700

    flyway

---
+++ src/main/resources/db/migration/V6__Create_init_user_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO db_spring_security.users
+    (id, email, name, password)
+VALUES (1, 'admin@gmail.com', 'admin', '$2a$10$OnkEW8dcR1EI7/465LOrHuw8Jc84nFqHC3ADcdKrQ/Pw3WIJoSK32');
+
+INSERT INTO db_spring_security.users
+    (id, email, name, password)
+VALUES (2, 'admin1@gmail.com', 'admin1', '$2a$10$a4Ty/MurU3Yv2HqpT5Gtge33YTvctopRsk8pxihi1eCzN/.kNHBEW');
+
+INSERT INTO db_spring_security.users
+    (id, email, name, password)
+VALUES (3, 'user@gmail.com', 'user', '$2a$10$UHuS0.vxkooEjwk7cyn/k..Flba2voIV3ZdFSw23V35eNfvhvQvWO');
